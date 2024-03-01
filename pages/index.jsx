42fd440c538656ad5e84fb2498e4daa4e50da51b
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2024-03-01 16:04:32 -0600

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Weather from "./components/weather";
+import Header from "./components/header";
+
+
+
+export default function Home() {
+  return (
+    <>
+    <Header />
+    <Weather />
+    </>
+  );
+}
