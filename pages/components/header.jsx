8409ad2fb62ef7f935b64b8f451abb937c7d8f63
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2024-03-02 14:47:29 -0600

    Update header.jsx

--- pages/components/header.jsx
+++ pages/components/header.jsx
@@ -11,18 +11,19 @@ export default function Header() {
 
     return (
       <>
-        <header className="flex w-full bg-white border-b-4 border-red-600">
+        <header className="flex w-full bg-white py-1 border-b-4 border-red-600">
 
        
-        <div className=" lg:hidden hamburger">
+        <div className="mx-2 lg:hidden hamburger">
             <div className="mt-5 w-7 my-0 mx-auto borderBar" onClick={handleShow}>
                 <div className="w-7 h-0.5 bg-black m-1.5 duration-75 barOne"></div>
                 <div className="w-7 h-0.5 bg-black m-1.5 duration-75 barTwo"></div>
                 <div className="w-7 h-0.5 bg-black m-1.5 duration-75 barThree"></div>
-
+            </div>
             <div>
             {Show &&
-                <ul className=" absolute block left-0 w-full h-screen bg-slate-50 text-black">
+                <div className='absolute block left-0 w-full h-full bg-slate-50 text-black'>
+                <ul className="">
                     <li className="text-lg font-semibold my-4 mx-4 py-4 border-t-4 border-b-4 border-black">Rent</li>
                     <li className="text-lg font-semibold my-4 mx-4 py-4 border-b-4 border-black">Manage Rental</li>
                     <li className="text-lg font-semibold my-4 mx-4 py-4 border-b-4 border-black">Buy</li>
@@ -30,9 +31,16 @@ export default function Header() {
                     <li className="text-lg font-semibold my-4 mx-4 py-4 border-b-4 border-black">Rewards</li>
                     <li className="text-lg font-semibold my-4 mx-4 py-4 border-b-4 border-black">Locations</li>
                     <li className="text-lg font-semibold my-4 mx-4 py-4 border-b-4 border-black">Business</li>
-                    <li className="text-lg font-semibold my-4 mx-4 py-4">Support</li>
-                </ul>}
+                    <li className="text-lg font-semibold my-4 mx-4 py-4  border-b-4 border-black">Support</li>
+
+                <div className="flex-auto">
+                <ul className='text-center'>
+                    <li className="inline-block text-lg font-semibold my-4 mx-8 font-bold">Login</li>
+                    <li className="inline-block text-lg font-semibold my-4 mx-8 font-bold">Join</li>
+                </ul>
             </div>
+                </ul>
+                </div>}
             </div>
         </div>
             <div className="flex-auto">
@@ -50,7 +58,7 @@ export default function Header() {
                     <li className="inline-block mx-4">Support</li>
                 </ul>
             </div>
-            <div className="flex-auto">
+            <div className="hidden  flex-auto lg:block ">
                 <ul>
                     <li className="inline-block my-4 mx-4 font-bold">Login</li>
                     <li className="inline-block my-4 mx-4 font-bold">Join</li>
