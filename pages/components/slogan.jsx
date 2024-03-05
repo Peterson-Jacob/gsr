81d735189076b8904e6b040952da02f7419d8a48
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2024-03-05 15:27:33 -0600

    Update slogan.jsx

--- pages/components/slogan.jsx
+++ pages/components/slogan.jsx
@@ -6,10 +6,10 @@ export default function Slogan(){
     return(
         <>
             <div className="w-full slogan">
-                <div className="pt-32 w-11/12 my-0 mx-auto">
-                    <h1 className="text-center text-5xl font-bold">Transform surroundings, not benchmarks.</h1>
+                <div className="w-full pt-24 lg:pt-32 lg:w-11/12 my-0 mx-auto">
+                    <h1 className="text-center text-3xl sm:text-5xl font-bold">Transform surroundings, not benchmarks.</h1>
 
-                    <p className="pt-6 text-4xl text-center w-5/6 my-0 mx-auto callAction">Now is the moment to seize back your time for active endeavors. 
+                    <p className="pt-6 text-xl sm:text-4xl text-center w-5/6 my-0 mx-auto callAction">Now is the moment to seize back your time for active endeavors. 
                     Whether an extended getaway along scenic routes or urban landscapes during work travels, we're here to 
                     assist you on any journey, in any vehicle of your choice. Let's embark on the adventure!</p>
                 </div>
