17d33cb006341b23b8ccc62062b7c256f9a88bdb
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2024-03-08 15:48:13 -0600

    Create card-data.js

---
+++ pages/components/card-data.js
@@ -0,0 +1,28 @@
+export const cardList = [{
+    name: 'one',
+    header: 'AAA memebers: Save up to 50% off rates.*',
+    action: 'Great moments start here. Lets Go!',
+    select: 'Book Now',
+  
+},
+{
+    name: 'two',
+    header: 'Unleash adventure with a Jeep',
+    action: 'where rugged capability meets the open road in the ultimate journey of freedom.',
+    select: 'Explore'
+   
+},
+{
+    name: 'three',
+    header: 'Elevate your experiences, reap the rewards',
+    action: 'Unleash a world of benefits with every step forward for your business.',
+    select: 'Explore Business Rewards'
+    
+},
+{
+    name: 'four',
+    header: 'Earn towards free retals and more with Gash Rewards.',
+    action: 'Gash Rewards members benfit from the start.',
+    select: 'Explore Rewards'
+}
+]
