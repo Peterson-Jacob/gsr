2beec21b96acb5c498ed530d14efce8e34740e66
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2024-03-11 16:00:58 -0500

    Update card-section.jsx

--- pages/components/card-section.jsx
+++ pages/components/card-section.jsx
@@ -10,23 +10,22 @@ import { cardList } from './card-data';
 function Part({index}){
     
     let card = cardList[index];
-    const image = [bmw, jeep, hyun, mazda];
-    
+    const image = [bmw, jeep, hyun, mazda, bmw];
 
     return(
 
-        <div className="relative w-full my-12 mx-auto bg-white rounded shadow-md shadow-black overflow-hidden sm:w-64  card">
-            <Image src={image[index]} alt={card.image} className="w-full h-64 lg:h-40"/>
-        <div className='bg-red-600 w-full h-1'></div>
+    <div className="relative w-full my-12 mx-auto bg-white rounded shadow-md shadow-black overflow-hidden sm:w-2/5 lg:w-64  card">
+        <Image src={image[index]}  className="w-full h-64 lg:h-40"/>
+    <div className='bg-red-600 w-full h-1'></div>
 
-        <div className='w-11/12 my-0 mx-auto'>
-            <h2 className='text-xl font-bold my-4'>{card.header}</h2>
-            <p className='my-4 pb-4'>{card.action}</p>
-        </div>
+    <div className='w-11/12 my-0 mx-auto'>
+        <h2 className='text-xl font-bold my-4'>{card.header}</h2>
+        <p className='my-4 pb-4'>{card.action}</p>
+    </div>
 
-        <div className='bg-slate-100 w-11/12 my-0 mx-auto h-0.5'></div>
-            <p className='py-3 text-center text-blue-500 text-xl '>{card.select}</p>
-        </div>
+    <div className='bg-slate-100 w-11/12 my-0 mx-auto h-0.5'></div>
+    <p className='py-3 text-center text-blue-500 text-xl '>{card.select}</p>
+ </div>
     )
 }
 
@@ -38,6 +37,8 @@ export default function Cardselect(){
     const [div2Clicked, setDiv2Clicked] = useState(false);
     const [div3Clicked, setDiv3Clicked] = useState(false);
     const [div4Clicked, setDiv4Clicked] = useState(false);
+    const [firstTwoClicked, setFirstClicked] = useState(true);
+    const [lastTwoClicked, setLastClicked] = useState(false);
     
 
     function oneClick(){
@@ -72,7 +73,23 @@ export default function Cardselect(){
         setDiv2Clicked(false);
         setDiv3Clicked(false);
         setDiv4Clicked(true);
+        
       }
+
+    function firstTwo(){
+        
+        setIndex(0);
+        setFirstClicked(true);
+        setLastClicked(false);
+        
+    }
+    
+    function lastTwo() {
+        setIndex(2);
+        setLastClicked(true);
+        setFirstClicked(false);
+        
+    };
 
     return(
         <>
@@ -80,20 +97,36 @@ export default function Cardselect(){
                 <div>
                     <h1 className="text-center text-3xl pt-10 px-4 lg:text-5xl">Wander off path, Embrace the Journey.</h1>
                 </div>
-
-                <div className='flex flex-wrap lg:flex-inline w-11/12 my-0 mx-auto lg:w-full'>
-                        <Part index={index} />
-                        
-
+                
+                <div className='flex flex-wrap w-11/12 my-0 mx-auto sm:hidden '>
+                        <Part index={index} />       
                 </div>
+                <div className='hidden flex-wrap w-11/12 my-0 mx-auto sm:flex lg:hidden'>
+                
+                            <Part index={index} />
+                            <Part index={index + 1} />       
+                </div>
+                    <div className='hidden  flex-wrap w-11/12 my-0 mx-auto lg:flex lg:flex-inline lg:w-full'>
+                            <Part index={0} />
+                            <Part index={1} />   
+                            <Part index={2} />   
+                            <Part index={3} />       
+                    </div> 
+                
 
-                <div className='w-1/3 my-4 mx-auto pb-12 flex md:hidden'>
+                <div className='w-1/3 my-4 mx-auto pb-12 flex sm:hidden'>
                     <div className={`w-4 h-4 my-0 mx-auto rounded-full cursor-pointer ${div1Clicked ? 'bg-red-500' : 'bg-black'}`}  onClick={oneClick}></div>
                     <div className={`w-4 h-4 my-0 mx-auto rounded-full cursor-pointer ${div2Clicked ? 'bg-red-500' : 'bg-black'}`} onClick={twoClick} ></div>
                     <div className={`w-4 h-4 my-0 mx-auto rounded-full cursor-pointer ${div3Clicked ? 'bg-red-500' : 'bg-black'}`} onClick={threeClick}> </div>
                     <div className={`w-4 h-4 my-0 mx-auto rounded-full cursor-pointer ${div4Clicked ? 'bg-red-500' : 'bg-black'}`} onClick={fourClick}></div>
                 
                 </div>
+
+                <div className='hidden w-1/3 my-4 mx-auto pb-12  sm:flex lg:hidden'>
+                    <div className={`w-4 h-4 my-0 mx-auto rounded-full cursor-pointer ${firstTwoClicked ? 'bg-red-500' : 'bg-black'}`}  onClick={firstTwo}></div>
+                    <div className={`w-4 h-4 my-0 mx-auto rounded-full cursor-pointer ${lastTwoClicked ? 'bg-red-500' : 'bg-black'}`} onClick={lastTwo} ></div>
+                
+                </div> 
             </div>
         </>
     )
