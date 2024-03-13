4ea39a63a603eaab8265be17183d0bf6cab64cf5
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2024-03-13 16:55:03 -0500

    Create rewards-section.jsx

---
+++ pages/components/rewards-section.jsx
@@ -0,0 +1,26 @@
+
+
+
+export default function Rewards(){
+
+    return(
+        <>
+            <div className="">
+                <h1 className="w-10/12 mt-24 mb-8 mx-auto text-center text-black text-5xl font-bold rewardHeader">Benefits from the start with Gash Rental Rewards!</h1>
+
+                <p className="w-10/12 my-8 mx-auto text-4xl text-center font-light  rewardAction">
+                Discover the excitement as you bypass the counter and go straight to your vehicle.
+                 Earn points along the way, bringing you closer to free rental days and exciting upgrades.* </p>
+
+                <div className="w-10/12 py-4 my-6 mx-auto bg-red-600 ">
+                    <p className="text-center text-white text-2xl font-bold">Join for Free</p>
+                </div>
+
+                <p className="w-11/12 mt-0 mb-24 mx-auto text-center text-sm ">
+                    *Skip the counter available at select locations. Upgrades are subject to availability. Additional terms 
+                    and exclusions apply. Point redemptions apply to per daytime and mileage charges only. Taxes, fees and 
+                    options are excluded. Terms and exclusions apply.</p>
+            </div>
+        </>
+    )
+}
