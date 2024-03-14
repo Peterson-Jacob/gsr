ba780cb51c794735b2b61073b837d8f23d590813
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2024-03-14 17:06:37 -0500

    Update rewards-section.jsx

--- pages/components/rewards-section.jsx
+++ pages/components/rewards-section.jsx
@@ -5,18 +5,18 @@ export default function Rewards(){
 
     return(
         <>
-            <div className="">
-                <h1 className="w-10/12 mt-24 mb-8 mx-auto text-center text-black text-5xl font-bold rewardHeader">Benefits from the start with Gash Rental Rewards!</h1>
+            <div className="max">
+                <h1 className="w-10/12 mt-24 mb-4 mx-auto text-center text-black text-3xl font-bold sm:text-5xl sm:mb-8   rewardHeader">Benefits from the start with Gash Rental Rewards!</h1>
 
-                <p className="w-10/12 my-8 mx-auto text-4xl text-center font-light  rewardAction">
+                <p className="w-10/12 mt-5 mb-8 mx-auto text-center font-normal text-xl sm:text-4xl lg:font-light lg:mt-8 lg:mb-10  2xl:font-normal rewardAction">
                 Discover the excitement as you bypass the counter and go straight to your vehicle.
                  Earn points along the way, bringing you closer to free rental days and exciting upgrades.* </p>
 
-                <div className="w-10/12 py-4 my-6 mx-auto bg-red-600 ">
+                <div className="w-10/12 py-4 my-6 mx-auto bg-red-600 lg:w-96 lg:py-6 ">
                     <p className="text-center text-white text-2xl font-bold">Join for Free</p>
                 </div>
 
-                <p className="w-11/12 mt-0 mb-24 mx-auto text-center text-sm ">
+                <p className="w-11/12 mt-0 mb-24 mx-auto text-center text-sm lg:w-2/5 lg:mt-10 lg:mb-16 ">
                     *Skip the counter available at select locations. Upgrades are subject to availability. Additional terms 
                     and exclusions apply. Point redemptions apply to per daytime and mileage charges only. Taxes, fees and 
                     options are excluded. Terms and exclusions apply.</p>
