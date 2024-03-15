b03baf1aa7016b545e2062bf09428effd51da25d
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2024-03-15 16:43:42 -0500

    Create closer.jsx

---
+++ pages/components/closer.jsx
@@ -0,0 +1,36 @@
+
+
+export default function Close(){
+
+    return(
+        <>
+            <div  className="w-full pt-6 pb-24 bg-white max">
+                
+                <h1 className="w-11/12 my-4 mx-auto text-2xl lg:text-3xl ">Effortlessly rent a car for every one of your Let’s Go occasions.</h1>
+
+                <div className="w-11/12 my-4 mx-auto ">
+                    <h2 className="text-lg my-2 font-bold lg:text-xl">Following wherever the road may take you.</h2>
+                    
+                    <p className="text-base lg:text-lg">No matter where you're headed, we provide top-rated vehicles tailored 
+                    to suit every traveler's requirements. Whether you're renting from our local 
+                    branches or airport outlets, select from a diverse array of options including 
+                    electric vehicles, luxury sedans, sports cars, SUVs, trucks, vans, and beyond.</p>
+                </div>
+                <div className="w-11/12 my-4 mx-auto">
+                    <h2 className="text-lg my-2 font-bold lg:text-xl">Start reaping the rewards and hit the road sooner.</h2>
+                    <p className="text-base lg:text-lg"> 
+                    Accrue points towards complimentary rental days and upgrades as you bypass the 
+                    counter and head straight to your car.* Simply drop off the keys upon return and 
+                    be on your way. Moreover, the more you rent, the more you'll earn, accelerating 
+                    your progression towards enhanced loyalty tier status.</p>
+                </div>
+                    <h3 className="w-11/12 my-4 mx-auto text-lg text-blue-500 lg:text-xl">Join Gash Rewards Today</h3>
+
+                    <p className="w-11/12 my-0 mx-auto text-sm lg:text-base">*Skip the counter option is offered at specific locations. Availability of upgrades
+                    may vary. Additional terms and exclusions may apply. Points can be redeemed towards 
+                    daytime and mileage charges only. Taxes, fees, and optional extras are not included. 
+                    Terms and exclusions may apply.</p>
+            </div>
+        </>
+    )
+}
