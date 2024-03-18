71624dc134bacb063ebce843ef737fce2a9a71ef
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2024-03-18 16:48:21 -0500

    Update card-data.js

--- pages/components/card-data.js
+++ pages/components/card-data.js
@@ -1,26 +1,26 @@
 export const cardList = [{
-    name: 'one',
+    name: 'BMW',
     header: 'AAA memebers: Save up to 50% off rates.*',
     action: 'Great moments start here. Lets Go!',
     select: 'Book Now',
   
 },
 {
-    name: 'two',
+    name: 'jeep',
     header: 'Unleash adventure with a Jeep',
     action: 'where rugged capability meets the open road in the ultimate journey of freedom.',
     select: 'Explore'
    
 },
 {
-    name: 'three',
+    name: 'hyundi',
     header: 'Elevate your experiences, reap the rewards',
     action: 'Unleash a world of benefits with every step forward for your business.',
     select: 'Explore Business Rewards'
     
 },
 {
-    name: 'four',
+    name: 'mazda',
     header: 'Earn towards free retals and more with Gash Rewards.',
     action: 'Gash Rewards members benfit from the start.',
     select: 'Explore Rewards',
