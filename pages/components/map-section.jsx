bc43e0e140c1f775a055307bbb529d5660404294
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2024-03-20 17:32:56 -0500

    Update map-section.jsx

--- pages/components/map-section.jsx
+++ pages/components/map-section.jsx
@@ -5,11 +5,11 @@ export default function Map(){
 
     return(
         <>
-            <div className='relative mb-56 lg:mb-80'>
+            <div className='relative mb-48 lg:mb-80'>
                 <Image src={map} alt="map of southeastern United States" className="w-full h-80 lg:h-96 map"/>
 
             
-                <div className='absolute top-20 left-10 right-10 bg-white shadow-lg shadow-black mx-auto py-4 lg:top-40 lg:left-20 lg:right-20 lg:w-3/5 mapMax'>
+                <div className='absolute top-20 left-0 right-0 w-11/12 bg-white shadow-lg shadow-black mx-auto py-4 lg:top-40 lg:left-20 lg:right-20 lg:w-3/5 mapMax'>
                     <h1 className='w-11/12 text-center text-3xl font-bold mt-10 mb-4 mx-auto sm:text-5xl sm:mb-8'>Perfect journey, Conveniently close.</h1>
                     <p className='text-center text-xl w-11/12 my-0 mx-auto font-normal sm:text-3xl sm:w-5/6 lg:w-3/5'>With countless local destinations across the country, we're just a stone's throw away from you</p>
                     <div className='w-11/12 mt-8 mb-12 mx-auto py-4 border-2 border-black text-center text-2xl font-semibold sm:w-80 lg:w-96 lg:py-6'><p>Nerby Locations</p></div>
