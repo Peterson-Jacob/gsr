7441d92011c3d75987e39ef07e18bd81e652cb6f
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2024-03-21 15:42:26 -0500

    Update map-section.jsx

--- pages/components/map-section.jsx
+++ pages/components/map-section.jsx
@@ -12,7 +12,7 @@ export default function Map(){
                 <div className='absolute top-20 left-0 right-0 w-11/12 bg-white shadow-lg shadow-black mx-auto py-4 lg:top-40 lg:left-20 lg:right-20 lg:w-3/5 mapMax'>
                     <h1 className='w-11/12 text-center text-3xl font-bold mt-10 mb-4 mx-auto sm:text-5xl sm:mb-8'>Perfect journey, Conveniently close.</h1>
                     <p className='text-center text-xl w-11/12 my-0 mx-auto font-normal sm:text-3xl sm:w-5/6 lg:w-3/5'>With countless local destinations across the country, we're just a stone's throw away from you</p>
-                    <div className='w-11/12 mt-8 mb-12 mx-auto py-4 border-2 border-black text-center text-2xl font-semibold sm:w-80 lg:w-96 lg:py-6'><p>Nerby Locations</p></div>
+                    <div className='w-11/12 mt-8 mb-12 mx-auto py-4 border-2 border-black text-center hover:bg-red-600 hover:text-white text-2xl font-semibold sm:w-80 lg:w-96 lg:py-6 cursor-pointer'><p>Nerby Locations</p></div>
                 </div>
         
             </div>
